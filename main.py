886733d899402b430eb9dac8986eebe4856054d9
j-nordin <57144825+j-nordin@users.noreply.github.com>
2022-06-02 14:05:33 +0200

    Added logging and error handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import os
 from dotenv import load_dotenv
 from pyoverkiz.const import SUPPORTED_SERVERS
@@ -9,9 +10,18 @@ import WindFetcher
 from OverkizExtensions import OverkizClientExtension
 
 
-def load_env_variables():
+def _load_env_variables():
     dotenv_file = os.path.join(os.path.dirname(__file__), 'env_variables.env')
     load_dotenv(dotenv_file)
+
+
+def _config_logging():
+    logfile = os.path.join(os.path.dirname(__file__), "logfile.log")
+    logging.basicConfig(filename=logfile,
+                        filemode="w", # a - append logs, w - rewrite logs
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        datefmt='%Y-%m-%d %X',
+                        level=logging.INFO)
 
 
 async def close_awning_if_unsafe_wind(username,
@@ -21,55 +31,68 @@ async def close_awning_if_unsafe_wind(username,
                                       unsafe_wind_gust_speed):
     async with OverkizClientExtension(username, password, server=SUPPORTED_SERVERS["somfy_europe"]) as client:
         try:
+            logging.info("Logging in to somfy...")
             await client.login()
-        except Exception as exception:  # pylint: disable=broad-except
-            print(exception)
-            return
+            logging.info("Login successful")
+        except Exception:  # pylint: disable=broad-except
+            logging.exception("Client could not log in")
 
-        # await client.refresh_states()
-        awning_states = await client.get_state(spa_awning_url)
-        for s in awning_states:
-            if s.name == 'core:OpenClosedState':
-                awning_is_open = s.value != 'closed'
+        try:
+            logging.info("Checking awning current state...")
+            # await client.refresh_states()
+            awning_states = await client.get_state(spa_awning_url)
+            for s in awning_states:
+                if s.name == 'core:OpenClosedState':
+                    awning_is_open = s.value != 'closed'
+            logging.info(f"Awning is currently: {'open' if awning_is_open else 'closed'}")
+        except Exception:
+            logging.exception("Awning-state could not be checked")
 
+        logging.info("Checking current weather conditions...")
         current_wind = WindFetcher.fetch_current_wind()
+        logging.info(f"Current weather conditions: {current_wind}")
 
         if (current_wind['wind_speed'] > unsafe_wind_speed or
             current_wind['wind_gust_speed'] > unsafe_wind_gust_speed) and awning_is_open:
             try:
-                res = await client.execute_raw_commands(device_url=spa_awning_url,
-                                                        commands=[Command("close")],
-                                                        notification_type_mask=1,
-                                                        notification_condition='ALWAYS',
-                                                        notification_text='Closing the spa awning due to strong wind',
-                                                        notification_title='Closing spa awning',
-                                                        execution_mode='highPriority')
-                print(res)
+                logging.info("Awning is being closed due to hard wind...")
+                await client.execute_raw_commands(device_url=spa_awning_url,
+                                                  commands=[Command("close")],
+                                                  execution_mode='highPriority')
+                logging.info("Command to close awning successfully sent")
+            except Exception:
+                PushNotifications.send_push_notification(title="Markis kunde inte fällas in",
+                                                         message="Kommando för att stänga markisen kunde inte skickas")
+                logging.exception("Command to close awning could not be sent")
+
+            try:
+                logging.info("Send push notification about closing the awning")
                 PushNotifications.send_push_notification(title="Markis fälls in",
                                                          message=f"Markisen fälls in på grund av hård vind. Nuvarande vind: "
                                                                  f"{current_wind['wind_speed']} m/s ({current_wind['wind_gust_speed']} m/s)")
-            except Exception as e:
-                print(e)
+            except Exception:
+                logging.exception("Push notification about closing the awning could not be sent")
         else:
-            print(f"Did not close awning, wind is only {current_wind}")
-
-        # while True:
-        #    events = await client.fetch_events()
-        #    print(events)
-        #    time.sleep(2)
+            logging.info(
+                f"Did not close awning, wind is {current_wind} and state of the awning is: {'open' if awning_is_open else 'closed'}")
 
 
 async def main() -> None:
-    load_env_variables()
+    _load_env_variables()
+    _config_logging()
     username = os.environ["USERNAME"]
     password = os.environ["PASSWORD"]
     spa_awning_url = os.environ["SPA_AWNING_ID"]
 
-    await close_awning_if_unsafe_wind(username,
-                                      password,
-                                      spa_awning_url,
-                                      unsafe_wind_speed=float(os.environ["UNSAFE_WIND_SPEED"]),
-                                      unsafe_wind_gust_speed=float(os.environ["UNSAFE_WIND_GUST_SPEED"]))
+    try:
+        await close_awning_if_unsafe_wind(username,
+                                          password,
+                                          spa_awning_url,
+                                          unsafe_wind_speed=float(os.environ["UNSAFE_WIND_SPEED"]),
+                                          unsafe_wind_gust_speed=float(os.environ["UNSAFE_WIND_GUST_SPEED"]))
+    except Exception:
+        logging.exception("Something while checking the awning went wrong")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
